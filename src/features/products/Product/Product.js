fe7635a368494a00f4d06599aa189162ada212c3
Nguyen Thanh Kiet <kiet13.itus@gmail.com>
2021-11-05 22:16:31 +0700

    Add hover effect to product item

--- src/features/products/Product/Product.js
+++ src/features/products/Product/Product.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import styles from './Product.module.scss'
 import { formatPrice } from '../../../utils/helpers'
+import { SearchButton } from '../../../components/Buttons'
+import { useNavigate } from 'react-router-dom'
 
-export default function Product({ image, name, price }) {
+export default function Product({ id, image, name, price }) {
+  const navigate = useNavigate()
+
   return (
-    <div className={styles.Product}>
-      <img className={styles.ProductImage} src={image} alt='product' />
+    <div className={styles.Product} onClick={() => navigate(`/products/${id}`)}>
+      <div className={styles.ProductImage}>
+        <img src={image} alt='product' />
+        <SearchButton className={styles.ProductSearch} />
+      </div>
+
       <p className={styles.ProductInfo}>
         <span className={styles.ProductName}>{name}</span>
         <span className={styles.ProductPrice}>{formatPrice(price)}</span>
